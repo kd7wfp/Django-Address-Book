f9ff6f4a09dd8c99d264644eb449df6762de466f
tanderson <tanderson@ancestry.com>
2019-07-01 23:14:56 -0600

    Add Tests. Added a root redirect. Served up the ReadMe.

--- AddressBook/views.py
+++ AddressBook/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Hello, world.")
+    f = open(os.path.abspath(os.path.curdir) + '\README.md', 'r')
+    file_content = f.read()
+    # file_content = os.path.abspath(os.path.curdir)
+    f.close()
+    return HttpResponse(file_content, content_type="text/plain")
