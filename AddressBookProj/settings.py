8cbe7385816618a8c6d9ecda6b243f5f0e6cb053
tanderson <tanderson@ancestry.com>
2019-07-01 21:27:25 -0600

    Contact Datbase Created

--- AddressBookProj/settings.py
+++ AddressBookProj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'AddressBook.apps.AddressBookConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'AddressBook.urls'
+ROOT_URLCONF = 'AddressBookProj.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'AddressBook.wsgi.application'
+WSGI_APPLICATION = 'AddressBookProj.wsgi.application'
 
 
 # Database
